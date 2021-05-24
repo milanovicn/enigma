f42820f16a57de2efb157b3ad8f5a2a6b3810e9a
e5598277 <nikola.vitorovic@fisglobal.com>
2021-05-24 17:45:28 +0200

    Fixed deleteTerm issue.

--- dictionary/src/main/java/enigma/dictionary/repository/TermRepository.java
+++ dictionary/src/main/java/enigma/dictionary/repository/TermRepository.java
@@ -2,13 +2,24 @@ package enigma.dictionary.repository;
 
 import enigma.dictionary.model.Term;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
+import javax.transaction.Transactional;
 import java.util.Optional;
 
 public interface TermRepository extends JpaRepository<Term, Long> {
 
     Term findTermByTitle(String title);
 
+    @Transactional
+    @Modifying
+    @Query("DELETE FROM Term t WHERE t.term_ID = :termId")
+    void deleteTermById(@Param("termId") Long termId);
+
+    @Query("SELECT t FROM Term t WHERE t.term_ID = :termId")
+    Term findTermById(@Param("termId") Long termId);
 
 }
