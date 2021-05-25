4ac828cf8b864a0709f9c30622cf67acac162820
aleksandra11maksimovic <maksimovic.aleksandra@outlook.com>
2021-05-25 11:35:44 +0200

    New data added.

--- dictionary/src/main/java/enigma/dictionary/model/Term.java
+++ dictionary/src/main/java/enigma/dictionary/model/Term.java
@@ -17,10 +17,10 @@ public class Term {
     @Column(name = "title", nullable = false)
     private String title;
 
-    @Column(name = "description", nullable = false, length = 30000)
+    @Column(name = "description", nullable = false, length = 6000)
     private String description;
 
-    @Column(name = "details", nullable = false, length = 30000)
+    @Column(name = "details", nullable = false, length = 6000)
     private String details;
 
     @ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
